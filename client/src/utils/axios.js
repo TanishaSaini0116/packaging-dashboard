50f68252917a8eb834af751ef1c6343851f7ebb2
Tanisha Saini <ktanishasaini@gmail.com>
2026-04-08 19:37:36 +0530

    final api fix

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL:  "https://packaging-dashboard.onrender.com/api",
+  baseURL: "https://packaging-dashboard.onrender.com",
 });
 
 API.interceptors.request.use((config) => {
